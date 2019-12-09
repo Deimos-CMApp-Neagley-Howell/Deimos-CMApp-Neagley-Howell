42f0629889adf3be33a4ffdf349e37266a76f183
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-12-09 15:27:09 -0600

    afternoon push up

--- src/ContactManagementApp.java
+++ src/ContactManagementApp.java
@@ -4,10 +4,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class ContactManagementApp {
     final static String folder = "data";
     final static String fileName = "contacts.txt";
+
+    static Scanner sc = new Scanner(System.in);
 //    static Contact kenHowell = new Contact("Ken Howell", 2103153119);
 //static Contact kellseyNeagley = new Contact("Kellsey Neagley", 2103810439);
 //
@@ -22,24 +25,30 @@ public class ContactManagementApp {
 
         List<String> ListString = Arrays.asList(kenHowell.getName() + " | " + kenHowell.getContactNumber(), kellseyNeagley.getName() + " | " + kellseyNeagley.getContactNumber());
 
-        try{
-            if(!Files.exists(dataDirectory)){
+        try {
+            if (!Files.exists(dataDirectory)) {
                 Files.createDirectories(dataDirectory);
             }
-            if(!Files.exists(dataFile)){
+            if (!Files.exists(dataFile)) {
                 Files.createFile(dataFile);
             }
             Path filepath = Paths.get("data", "contacts.txt");
             Files.write(filepath, ListString);
 //            written a output below
-            List<String> updatedList = Files.readAllLines(filepath);
-            for (String contact : updatedList){
-                System.out.println(contact);
-            };
+//            List<String> updatedList = Files.readAllLines(filepath);
+//            for (String contact : updatedList){
+//                System.out.println(contact);
+//        };
 //            System.out.println("updatedList = " + updatedList);
         } catch(IOException e) {
             e.printStackTrace();
         }
+
+        System.out.printf("Welcome to your Contact Management Application!%n");
+        System.out.printf("1. View Contacts.%n2. Add a new contact.%n3. Search a contact by name.%n4. Delete an existing contact.%n5. Exit application.%n");
+        System.out.printf("Enter an option (1, 2, 3, 4 or 5):%n");
+        int userInput = sc.nextInt();
+        System.out.println("userInput = " + userInput);
 
 
 //        Path filepath = Paths.get("data", "contacts.txt");
